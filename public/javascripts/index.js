9155213cb5cf7063e04796846aaf9e022db47077
hsuregan <hsuregan@gmail.com>
2015-11-20 19:26:54 -0800

    counter

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,8 @@
 $(document).ready(
    function() {
     setInterval(function() {
-       $.getJSON("http://107.170.149.67:3000/", function(response) {
-        //console.log(response);
+       $.getJSON("http://107.170.149.67:3000/count", function(response) {
+        console.log(response);
         $('#count').html(response);
        }).fail()
      }, 500);
