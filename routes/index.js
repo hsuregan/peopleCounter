e8267a1b808e7dfe7a6bdc503921824387c810f1
hsuregan <hsuregan@gmail.com>
2015-11-20 22:24:54 -0800

    add gets

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ router.post('/', function(req, res, next) {
 	res.json(counter);
 })
 
+router.get('/increment', function(req, res, next) {
+	counter += 1;
+	res.json(counter);
+})
+
+router.get('/decrement', function(req, res, next) {
+	counter -= 1;
+	res.json(counter);
+})
+
+
 router.post('/increment', function(req, res, next) {
 	counter += 1;
 	res.json(counter);
