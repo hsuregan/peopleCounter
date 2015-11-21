94e5e0dda2d23716153f559e02a6692084987557
hsuregan <hsuregan@gmail.com>
2015-11-20 21:36:10 -0800

    add post to home page

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var counter = 0;
-var graph = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  graph.push(counter);
+  //graph.push(counter);
   res.render('index', { counter: counter });
 });
+
+router.post('/', function(req, res, next) {
+	res.json(counter);
+})
 
 router.post('/increment', function(req, res, next) {
 	counter += 1;
