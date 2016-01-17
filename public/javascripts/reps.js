ae817d8cd9571cea93a614412e3a6fd69f58346c
hsuregan <hsuregan@gmail.com>
2016-01-16 19:51:35 -0800

    get count

--- public/javascripts/reps.js
+++ public/javascripts/reps.js
@@ -1,6 +1,6 @@
 function getReps() {
 	setInterval(function() {
-     $.getJSON("http://107.170.149.67/reps/increment", function(response) {
+     $.getJSON("http://107.170.149.67/reps/count", function(response) {
       console.log(response);
       $('#reps').html(response);
      }).fail()
