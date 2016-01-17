a8bb180edf7af43cd9e5314f95f7adb5347f5bd5
hsuregan <hsuregan@gmail.com>
2016-01-17 02:38:40 -0800

    work again

--- public/javascripts/reps.js
+++ public/javascripts/reps.js
@@ -1,7 +1,7 @@
 function getReps() {
 	setInterval(function() {
 		//http://107.170.149.67/reps/count
-     $.getJSON("http://localhost:3000/reps/count", function(response) {
+     $.getJSON("http://107.170.149.67/reps/count", function(response) {
       console.log(response);
       $('#reps').html(response);
      }).fail()
